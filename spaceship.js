cd695624eb8a2c5b147fc197fcde3f6338bccfbb
morgan911 <sikachevsergey@gmail.com>
2016-02-18 19:54:58 +0200

    Fire added to space ship. Ship is used ad a global object. It should be refactored.

--- spaceship.js
+++ spaceship.js
@@ -86,13 +86,16 @@ var ship = {
 	friction:1/4,
 	rotateSpeed:5,
 	image:'./ship.png',
-	draw:function(){
+	fire: [new ShipFire(), new ShipFire()],
+	draw:function(){		
 		var image = new Image();
 		image.src = this.image;
     	var ctx = CANVAS.getContext('2d')
     	ctx.save();
     	ctx.translate(this.x, this.y);
     	ctx.rotate(Math.PI/180*this.angle);
+		this.fire[0].draw(ctx, -this.width/2+5, -this.height/2+10);
+		this.fire[1].draw(ctx, -this.width/2+5, this.height/2-20);
     	ctx.drawImage(image, -this.width/2, -this.height/2, this.width, this.height);
     	ctx.fillStyle = '#00FF00';
     	ctx.fillRect(0, 0, 1,1);
@@ -118,16 +121,51 @@ var ship = {
 			this.speed = 0;
 		this.x += Math.cos(Math.PI/180*this.angle)*this.speed;
 		this.y += Math.sin(Math.PI/180*this.angle)*this.speed;
+		for(var i = 0; i < this.fire.length; i++){
+			this.fire[i].update();
+		}
 	}
 }
 
+function ShipFire(){
+	this.tickCount = 0;
+	this.tickCount=0;
+    this.frameIndex=[0,0];	
+    this.ticksPerFrame=4;
+    this.framesCount=[5,4];
+    this.ship = ship;
+    this.image='./fire.png';
+    this.update = function(){
+        if (this.tickCount++ > this.ticksPerFrame){	
+    		if (++this.frameIndex[1] > this.framesCount[1] - 1) {
+    			this.frameIndex[1] = 0;
+    			if (++this.frameIndex[0] > this.framesCount[0] - 1) {
+    				this.frameIndex[0] = 0;
+    			}
+    		}
+    		this.tickCount = 0;
+    	}
+    };
+    this.draw = function(ctx, x, y){
+    	var image = new Image();
+    	image.src = this.image;	
+    		ctx.drawImage(
+    			image,
+    			this.frameIndex[0]*95,
+    			this.frameIndex[1]*95,
+    			95,95,
+    			x, y,
+    			10, 10   				
+    		);	
+    }
+}
 
 createCanvas();
 frame();
 window.addEventListener('keydown', function(e){Key.keyDown(e);} ,false);
 window.addEventListener('keyup', function(e){Key.keyUp(e);} ,false);
 
-//TODO: finish keyboard ship control.
+//TODO: create wearpon.
 //TODO: create asteroids.
 //TODO: create collision detection.
 //TODO: Implement camera.
